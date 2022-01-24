3045be4bf54015d4aecc1e02e62d74e975fe2961
Aurélien Gourmelen <aurelien.gourmelen@gmail.com>
2022-01-24 11:53:26 +0100

    étape 3 : insérer les produits dans la page d'accueil

---
+++ front/js/script.js
@@ -0,0 +1,27 @@
+//Ciblage de l'id items de la div section
+const itemsSection = document.getElementById("items");
+
+//Définition de l'URL de l'API dans une variable
+let url = "http://localhost:3000/api/products";
+
+    //Requête GET de l'API avec Fetch
+    fetch(url)
+    //Promesse
+    .then((response) =>
+    response.json()
+    .then(data => {
+        for (let product of data){
+        const products = `<a href="./product.html?id=${product._id}">
+        <article>
+            <img src="${product.imageUrl}" alt="${product.altTxt}">
+            <h3 class="productName">${product.name}</h3>
+            <p class="productDescription">${product.description}</p>
+        </article>`;
+        //Insertion du HTML dans le DOM
+        itemsSection.insertAdjacentHTML("beforeend", products);
+        }
+    })
+    )
+    // Si la promesse n'est pas résolue
+    .catch((err) => console.log("Erreur : " + err));
+
