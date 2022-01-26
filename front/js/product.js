95cecbd9710123c05b7bb6932c5593a6d47d1e94
Aurélien Gourmelen <aurelien.gourmelen@gmail.com>
2022-01-26 15:02:44 +0100

    correction script product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,41 +1,51 @@
-//Je récupère l'id du produit cliqué sur la page d'accueil
-let getProductId = new URLSearchParams(document.location.search);
-let productId = getProductId.get("id");
-console.log(productId);
+document.addEventListener("DOMContentLoaded", function () {
+    //Je récupère l'id du produit cliqué sur la page d'accueil
+    let getProductId = new URLSearchParams(document.location.search);
+    let productId = getProductId.get("id");
+    console.log(productId);
 
-//Je rentre l'url avec id dans une variable
-let urlProductId = `http://localhost:3000/api/products/${productId}`;
-console.log(urlProductId);
+    //Je rentre l'url avec id dans une variable
+    let urlProductId = `http://localhost:3000/api/products/${productId}`;
+    console.log(urlProductId);
 
-//Je fais une requête get de l'API
-fetch(urlProductId)
-    .then((response) =>
-    response.json().then((data) => {
-        //Je vérifie que j'ai bien accès aux données de l'api dans la console
-        console.log(data);
-        //Je récupère et affiche l'image
-        const productImg = `<img src="${data.imageUrl}">`;
-        document.querySelector(".item__img").insertAdjacentHTML("beforeend", productImg);
-        //Je récupère et affiche le titre
-        const productTitle = `${data.name}`;
-        document.querySelector("#title").insertAdjacentHTML("beforeend", productTitle);
-        //Je récupère et affiche le prix
-        const priceProduct = `${data.price}`;
-        document.querySelector("#price").insertAdjacentHTML("beforeend", priceProduct);
-        //Je récupère et affiche la description
-        const descProduct = `${data.description}`;
-        document.querySelector("#description").insertAdjacentHTML("beforeend", descProduct);
-        //Je récupère le tableau des couleurs + vérif en console
-        const tabColors = data.colors;
-        console.log(tabColors);
-        //Je crée une boucle pour récupérer toutes les couleurs puis je les affiche dans les options
-        let AddColors = "";
-        for (let i of tabColors) {
-        AddColors += `<option value="${i}"> ${i} </option>`;
-        }
-        document.querySelector("#colors").insertAdjacentHTML("beforeend", AddColors);
-    })
-    )
-    // Affichage de l'erreur si la promesse n'est pas résolue
-    .catch((err) => console.log("Erreur : " + err));
+    //Je fais une requête get de l'API
+    fetch(urlProductId)
+        .then((response) =>
+            response.json().then((data) => {
+                //Je vérifie que j'ai bien accès aux données de l'api dans la console
+                console.log(data);
+                //Je récupère et affiche les éléments img, alt title price et description dans le HTML
+                document.querySelector(
+                    ".item__img"
+                ).innerHTML += `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+                document.querySelector("#title").textContent += data.name;
+                document.querySelector("#price").textContent += data.price;
+                document.querySelector("#description").textContent +=
+                    data.description;
+                //Je récupère le tableau des couleurs que j'insère dans une constante
+                const tabColors = data.colors;
+                //Je sélectionne la balise select, d'identifiant colors
+                let colorId = document.querySelector("#colors");
+                //Je crée une boucle itérative et insertion dans le HTML
+                for (let color of tabColors) {
+                    let option = document.createElement("option");
+                    option.innerHTML = `${color}`;
+                    option.value = `${color}`;
+                    colorId.appendChild(option);
+                }
+            })
+        )
+        // Affichage de l'erreur si la promesse n'est pas résolue
+        .catch((err) => console.log("Erreur : " + err));
+});
 
+//---------------------------Ajout des produits dans le panier----------------------------------//
+
+//Sélection de l'id du produit
+
+//La ligne suivante s'exécute à chaque chargement - Ce n'est pas ce qu'on veut !!!
+//localStorage.getItem("");
+
+//Sélection de la quantité de produits
+
+//Sélection de la couleur du produit
--- front/js/script.js
+++ front/js/script.js
@@ -1,27 +1,26 @@
-//Ciblage de l'id items de la div section
+//Ciblage de l'id items
 const itemsSection = document.getElementById("items");
 
 //Définition de l'URL de l'API dans une variable
 let url = "http://localhost:3000/api/products";
 
-    //Requête GET de l'API avec Fetch
-    fetch(url)
+//Requête GET de l'API avec Fetch
+fetch(url)
     //Promesse
     .then((response) =>
-    response.json()
-    .then(data => {
-        for (let product of data){
-        const products = `<a href="./product.html?id=${product._id}">
-        <article>
-            <img src="${product.imageUrl}" alt="${product.altTxt}">
-            <h3 class="productName">${product.name}</h3>
-            <p class="productDescription">${product.description}</p>
-        </article>`;
-        //Insertion du HTML dans le DOM
-        itemsSection.insertAdjacentHTML("beforeend", products);
-        }
-    })
+        response.json().then((data) => {
+            for (let product of data) {
+                const products = `<a href="./product.html?id=${product._id}">
+                                    <article>
+                                        <img src="${product.imageUrl}" alt="${product.altTxt}">
+                                        <h3 class="productName">${product.name}</h3>
+                                        <p class="productDescription">${product.description}</p>
+                                    </article>
+                                    </a>`;
+                //Insertion du HTML dans le DOM
+                itemsSection.insertAdjacentHTML("beforeend", products);
+            }
+        })
     )
     // Si la promesse n'est pas résolue
     .catch((err) => console.log("Erreur : " + err));
-
